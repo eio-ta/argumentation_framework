df393c5f0c4d44caf6625ccf186040f245711bce
EL0DY <elodytang@hotmail.fr>
2022-12-15 19:37:36 +0100

    correct the error

--- class_af.py
+++ class_af.py
@@ -74,26 +74,42 @@ class ArgF:
         if(valide == []):
             valide = self.get_valide()
         invalide = []
-        for e in valide:
+
+        for e in ori:
             for a, b in self.attacks:
-                if(a == e):
-                    if(b not in invalide):
-                        invalide.append(b)
-                        if(b in valide):
-                            valide.remove(b)
-                        for c, d in self.attacks:
-                            if(c == b):
-                                if(d not in valide and d not in invalide):
-                                    valide.append(d)
+                if(a == e and b not in valide):
+                    invalide.append(b)
+            
+        for e in invalide:
+            for c, d in self.attacks:
+                if(c == e and d not in invalide and d not in valide):
+                    valide.append(d)
+
         if(valide == ori and len(ori) == 1 and self.is_well(ori[0])):
             return valide
         if(valide == ori and len(ori) == 1 and self.is_weak(ori[0])):
             return []
         return valide
+    
+    def defense_recursive(self, ori = []):
+        valide = ori
+        tmp_def = self.defense(ori)
+        while(valide != tmp_def):
+            if(self.is_cf(tmp_def)):
+                return None
+            else:
+                valide = tmp_def
+                tmp_def = self.defense(valide)
+        return valide
 
     def grounded(self):
         res = []
-        res.append(self.defense())
+        res.append(self.defense_recursive())
+        if(res == [None]):
+            res = []
+            res.append(self.get_valide())
+        for a in res:
+            a.sort()
         return res
 
     def completed(self):
@@ -104,13 +120,17 @@ class ArgF:
         for i in range(len(permutations)):
             tmp = list(permutations[i])
             if(self.is_cf(tmp) == False):
-                tmp_def = self.defense(tmp)
+                tmp_def = self.defense_recursive(tmp)
                 tmp_valide = self.get_valide()
-                if(all(elem in tmp_def for elem in tmp_valide)):
-                    if(tmp_def == tmp):
-                        res.append(tmp_def)
-        if(self.get_valide() == []):
-            res.append([])
+                if(tmp_def != None):
+                    if(all(elem in tmp_def for elem in tmp_valide)):
+                        if(tmp_def == tmp):
+                            res.append(tmp_def)
+
+        gr = self.grounded()
+        if(len(gr) == 1 and gr[0] not in res):
+            res += gr
+
         return res
 
     def preferred(self, completed):
